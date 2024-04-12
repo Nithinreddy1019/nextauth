c4814b0c9c677a1cdd21f568a0e01a0ff8ebc8fa
Nithinreddy1019 <kethireddynithinreddy@gmail.com>
2024-04-12 13:51:56 +0530

    added homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,36 @@
-import { Button } from "@/components/ui/button"
+import LoginButton from "@/components/auth/LoginButton";
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils"
+import {Poppins} from "next/font/google"
 
 
-export default function Home() {
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: ["600"]
+})
+
+export default async function Home() {
   return (
-    <div className="text-3xl font-bold">
-      Hello mate
-      <Button variant={"custom"}>Button</Button>
+    <main className="flex h-screen flex-col justify-center items-center bg-gradient-to-r from-blue-700  via-blue-500 via-50% to-blue-600  ">
 
-    </div>
+      <div className=" space-y-6 text-center">
+        <h1 className={cn("text-4xl font-semibold text-white drop-shadow-[0_3px_10px_rgb(0,0,0,0.2)]", font.className) }>
+          🔐Auth
+        </h1>
+        <p className="text-lg text-white font-medium">
+          An authentication application
+        </p>
+
+        <div>
+          <LoginButton>
+            <Button variant={"secondary"} size={"lg"} className="text-lg h-12 rounded-xl">
+              Sign in
+            </Button>
+          </LoginButton>
+        </div>
+      </div>
+
+    </main>
   );
 }
